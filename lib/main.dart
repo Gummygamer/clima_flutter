dced832a826418c9ac1427caf2fcb1884311bb70
André Castro <aisushin@gmail.com>
2021-04-10 19:03:26 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:clima_flutter/forecast.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'forecast.dart';
+import 'key.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+Future<FullResponse> fetchFullResponse() async {
+  final response = await http.get(Uri.https('api.hgbrasil.com', '/weather',
+      {'key': key, 'city_name': 'Fortaleza,CE'}));
+
+  if (response.statusCode == 200) {
+    return FullResponse.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Não foi possível carregar a previsão');
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<FullResponse> futureFullResponse;
+
+  @override
+  void initState() {
+    super.initState();
+    futureFullResponse = fetchFullResponse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clima flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Clima flutter'),
+        ),
+        body: Center(
+          child: FutureBuilder<FullResponse>(
+            future: futureFullResponse,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.results.forecast[0].date);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
